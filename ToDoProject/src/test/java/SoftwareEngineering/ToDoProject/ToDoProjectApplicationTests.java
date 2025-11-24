287592162b3db95842449125ffa7bd8dd03265c8
Katrinkgm <katrin.kgm@gmail.com>
2025-11-24 19:30:35 +0100

    Spring

---
+++ ToDoProject/src/test/java/SoftwareEngineering/ToDoProject/ToDoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package SoftwareEngineering.ToDoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

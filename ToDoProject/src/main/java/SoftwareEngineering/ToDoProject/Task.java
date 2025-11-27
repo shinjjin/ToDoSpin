206064b52bbadee58f7ad0c857cf96ed363c7c19
shinjin <147919339+shinjjin@users.noreply.github.com>
2025-11-27 14:43:02 +0100

    added getter and setter in task
    for id and name i

--- ToDoProject/src/main/java/SoftwareEngineering/ToDoProject/Task.java
+++ ToDoProject/src/main/java/SoftwareEngineering/ToDoProject/Task.java
@@ -4,4 +4,13 @@ public class Task {
     private long id;
     private String name;
     private boolean state;
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }

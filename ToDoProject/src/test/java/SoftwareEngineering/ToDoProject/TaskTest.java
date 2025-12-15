f6d1ef7d7f7293015747ae3d99c2880c6399595e
jonathanWue <167776642+jonathanWue@users.noreply.github.com>
2025-12-15 11:17:31 +0100

    Task Tests überarbeitet

--- ToDoProject/src/test/java/SoftwareEngineering/ToDoProject/TaskTest.java
+++ ToDoProject/src/test/java/SoftwareEngineering/ToDoProject/TaskTest.java
@@ -6,38 +6,71 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TaskTest {
 
     @Test
-    void constructorDefaults() {
+    void TaskgetsStartingIDTest() {
         Task task = new Task(1, "TestTask");
-        
         assertEquals(1, task.getId());
+    }
+
+    @Test
+    void TaskgetsStartingNameTest() {
+        Task task = new Task(1, "TestTask");
         assertEquals("TestTask", task.getName());
-        assertFalse(task.isDone());
-        assertFalse(task.isChosen());  
     }
 
     @Test
-    void constructorChosen() {
+    void TaskIsNotDoneByDefaultTest() {
+        Task task = new Task(1, "TestTask");
+        assertFalse(task.isDone());
+    }
+
+    @Test
+    void TaskIsNotChosenByDefaultTest() {
+        Task task = new Task(1, "TestTask");
+        assertFalse(task.isChosen());
+    }
+
+    @Test
+    void ChosenTaskHasRigthID() {
         Task task = new Task(2, "ChosenTask", true);
-        
         assertEquals(2, task.getId());
-        assertEquals("ChosenTask", task.getName());
-        assertFalse(task.isDone());
-        assertTrue(task.isChosen());  
     }
 
     @Test
-    void settersAndGetters() {
-        Task task = new Task(3, "OldName");
+    void ChosenTaskHasRigthName() {
+        Task task = new Task(2, "ChosenTask", true);
+        assertEquals("ChosenTask", task.getName());
+    }
 
+    @Test
+    void ChosenTaskIsNotDoneByDefault() {
+        Task task = new Task(2, "ChosenTask", true);
+        assertFalse(task.isDone());
+    }
+
+    @Test
+    void ChosenTaskIsChosen() {
+        Task task = new Task(2, "ChosenTask", true);
+        assertTrue(task.isChosen());
+    }
+
+    @Test
+    void ChangingTaskNamesWorks() {
+        Task task = new Task(3, "OldName");
         task.setName("NewName");
         assertEquals("NewName", task.getName());
-        
-        task.setDone(true);
-        assertTrue(task.isDone());
-
-        task.setChosen(true);
-        assertTrue(task.isChosen());  
     }
 
+    @Test
+    void TaskCanBeSetToTrue() {
+        Task task = new Task(3, "OldName");
+        task.setDone(true);
+        assertTrue(task.isDone());
+    }
 
+    @Test
+    void TaskCanBeChosen() {
+        Task task = new Task(3, "OldName");
+        task.setChosen(true);
+        assertTrue(task.isChosen());
+    }
 }
